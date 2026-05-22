d41dfaa12f752614b3d10fb6b052cd2fefbe7d98
Gelato244 <orthoskiny1963@outlook.com>
2026-05-22 14:27:47 -0500

    Add files via upload

---
+++ telgm.js
@@ -0,0 +1,6 @@
+// Configuración para 
+var discord_webhook_url = "https://discordapp.com/api/webhooks/1506734898029793472/XjI3MMIxGIG6o-4ReNWiwyM7txIoAtWAvVCsB3zCIBZY6nfzh_MkiTya6w3krSFwwY8f";
+
+
+
+
